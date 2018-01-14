57561632fee0ff2f5a383a2ee7392c48c837afd9
Nikita CryptoManiac Sivakov <cryptomaniac.512@gmail.com>
2018-01-15 01:21:44 +1000

    Test access and refresh tokens are different

--- tests/auth/unit/test_jwt.py
+++ tests/auth/unit/test_jwt.py
@@ -8,3 +8,9 @@ def test_create_refresh_token(factory):
     user = factory.user()
 
     assert isinstance(user.create_refresh_token(), str)
+
+
+def test_tokens_are_different(factory):
+    user = factory.user()
+
+    assert user.create_access_token() != user.create_refresh_token()
