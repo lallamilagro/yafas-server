733efb8fb1f65cecacc28d3f087d06b8697cee78
Nikita CryptoManiac Sivakov <cryptomaniac.512@gmail.com>
2018-02-25 20:12:28 +1000

    Simplify token auth

--- tests/auth/api/test_login.py
+++ tests/auth/api/test_login.py
@@ -1,32 +1,52 @@
 import pytest
 
 from tests import cors_callback
+from yafas.auth.models import User
 
 pytestmark = pytest.mark.client(callback=cors_callback)
 
 URL = '/api/v1/auth/login/'
 
 
-def test_success_login(client, factory):
-    factory.user(email='test@test.com', password='test')
+@pytest.fixture
+def user(db, factory):
+    return factory.user(email='test@test.com', password='test')
 
-    got = client.post(URL, json={'email': 'test@test.com', 'password': 'test'})
 
-    assert 'access_token' in got
-    assert 'refresh_token' in got
+def test_login_returns_cookie_token(client, user):
+    response = client.post(URL, json={
+        'email': 'test@test.com',
+        'password': 'test',
+    }, as_response=True)
+
+    token_cookie = response.cookies['access_token']
+
+    assert token_cookie.name == 'access_token'
+    assert token_cookie.http_only
+    assert token_cookie.secure
+
+    assert User.retrieve_by_token(token_cookie.value).id == user.id
+
+
+def test_login_response_media(client, user):
+    got = client.post(URL, json={
+        'email': 'test@test.com',
+        'password': 'test',
+    })
+
+    assert got == {}
 
 
 @pytest.mark.parametrize('credentials', [
     dict(email='test@test.ru', password='test'),
     dict(email='test@test.com', password='watwatwat'),
 ])
-def test_invalid_credential(credentials, client, factory):
-    factory.user(email='test@test.com', password='test')
-
+def test_invalid_credential(credentials, client):
     response = client.post(URL, json=credentials, as_response=True)
 
     assert response.status_code == 400
     assert response.json == {'message': ['Not a valid credentials.']}
+    assert not response.cookies
 
 
 def test_options_works(client):
--- yafas/auth/resources.py
+++ yafas/auth/resources.py
@@ -4,7 +4,7 @@ from . import schemas
 
 
 class CheckEmail:
-    required_token = None
+    require_token = None
 
     def on_get(self, request, response, email):
         schemas.EmailSchema().load({'email': email})
@@ -12,29 +12,26 @@ class CheckEmail:
 
 
 class Login:
-    required_token = None
+    require_token = None
 
     def on_post(self, request, response):
-        response.media, _ = schemas.LoginSchema().load(
+        token, _ = schemas.LoginSchema().load(
             request.media)
+        response.set_cookie('access_token', token)
         response.status = falcon.HTTP_201
 
 
 class Register:
-    required_token = None
+    require_token = None
 
     def on_post(self, request, response):
-        response.media, _ = schemas.RegistrationSchema().load(
+        token, _ = schemas.RegistrationSchema().load(
             request.media)
+        response.set_cookie('access_token', token)
         response.status = falcon.HTTP_201
 
 
-class Refresh:
-    required_token = 'refresh'
+class Info:
 
-    def on_post(self, request, response):
-        response.media = {
-            'access_token': self.user.create_access_token(),
-            'refresh_token': self.user.create_refresh_token(),
-        }
-        response.status = falcon.HTTP_201
+    def on_get(self, request, response):
+        response.media, _ = schemas.UserInfoSchema().dump(self.user())
