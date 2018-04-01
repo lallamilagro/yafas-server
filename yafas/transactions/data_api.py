e724f063327652c81e15d1f54db50bc9f2cc3914
Nikita CryptoManiac Sivakov <cryptomaniac.512@gmail.com>
2018-04-02 01:03:34 +1000

    Refactor transactions working with data

---
+++ yafas/transactions/data_api.py
@@ -0,0 +1,13 @@
+from yafas.base.data_api import DataApi
+
+from . import models, schemas
+
+
+class TransactionApi(DataApi):
+
+    model = models.Transaction
+    serializers = {
+        'retrieve': schemas.TransactionSchema(),
+        'create': schemas.TransactionCreateSchema(),
+        'update': schemas.TransactionCreateSchema(),
+    }
--- yafas/transactions/resources.py
+++ yafas/transactions/resources.py
@@ -1,36 +1,36 @@
 import falcon
 
-from . import schemas
+from .data_api import TransactionApi
 
 
 class Transaction:
 
+    @staticmethod
+    def data_api(id, request):
+        return TransactionApi(id=id, user_id=request.user_id)
+
     def on_get(self, request, response, id: int):
-        response.media, _ = schemas.TransactionRetrieveSchema().load(dict(
-            id=id, user_id=self.user().id,
-        ))
+        response.media = self.data_api(id, request).retrieve()
 
     def on_put(self, request, response, id: int):
-        response.media, _ = schemas.TransactionUpdateSchema().load(dict(
-            user_id=self.user().id, id=id, **request.media,
-        ))
+        response.media = self.data_api(id, request).update(
+            **request.media,
+        )
 
     def on_delete(self, request, response, id: int):
-        schemas.TransactionDeleteSchema().load(dict(
-            user_id=self.user().id, id=id,
-        ))
+        self.data_api(id, request).delete()
         response.status = falcon.HTTP_204
 
 
 class Transactions:
 
+    @staticmethod
+    def data_api(request):
+        return TransactionApi(user_id=request.user_id)
+
     def on_post(self, request, response):
-        response.media, _ = schemas.TransactionCreateSchema().load(dict(
-            user_id=self.user().id, **request.media,
-        ))
+        response.media = self.data_api(request).create(**request.media)
         response.status = falcon.HTTP_201
 
     def on_get(self, request, response):
-        response.media, _ = schemas.TransactionListSchema().load(
-            {'user_id': self.user().id},
-        )
+        response.media = self.data_api(request).list()
